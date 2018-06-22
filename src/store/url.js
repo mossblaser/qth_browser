a86c95fa26a33648ff4a67d06f5b1a4d7614de80
Jonathan Heathcote <mail@jhnet.co.uk>
2018-06-22 07:09:21 +0100

    Load last used server from cookie if not in URL.

--- src/store/url.js
+++ src/store/url.js
@@ -87,6 +87,30 @@ const updateStateToMatchURL = (store) => {
 }
 
 /**
+ * Store the current host in a cookie.
+ */
+const updateCookie = (host) => {
+	if (host !== null) {
+		document.cookie = `qthHost=${encodeURIComponent(host)}; expires=Fri, 31 Dec 9999 23:59:59 GMT`
+	}
+}
+
+const cookieRegex = /(?:^|;)qthHost=([^;]+)(?:;|$)/;
+
+/**
+ * Load the host from a cookie (if available).
+ */
+const readCookie = () => {
+	const match = cookieRegex.exec(document.cookie);
+	if (match) {
+		const host = decodeURIComponent(match[1]);
+		return host;
+	} else {
+		return null;
+	}
+}
+
+/**
  * Lock the hash-part of the page URL to the most important parts of the state.
  * Specifically, URLs will be produced with the following format:
  *
@@ -99,16 +123,32 @@ const updateStateToMatchURL = (store) => {
  * If the state changes,the URL will be modified to match, pushing the state
  * into the browser history. If the URL changes, the state will be updated to
  * match.
+ *
+ * Finally, the server name is recorded in a cookie whenever it changes in the
+ * state. When lockStateAndUrl is first called, if no URL is provided, the
+ * cookie is used to set the Qth host. If the cookie is not set, the Qth host
+ * is not set either.
  */
 const lockStateAndUrl = (store) => {
 	// Update the URL when state changes
 	store.subscribe(() => updateURLToMatchState(store));
 	
+	// Update the cookie too!
+	store.subscribe(() => updateCookie(store.getState().qth.host));
+	
 	// Update state in response to URL change
 	window.addEventListener("hashchange", () => updateStateToMatchURL(store));
 	
-	// Initially go where the URL tells us
-	updateStateToMatchURL(store);
+	// Initially go where the URL tells us, unless the URL is empty in which case
+	// load from a cookie.
+	if (window.location.hash && window.location.hash.length > 1) {
+		updateStateToMatchURL(store);
+	} else {
+		const cookieHost = readCookie();
+		if (cookieHost) {
+			store.dispatch(qthActions.connect(cookieHost));
+		}
+	}
 }
 
 export default lockStateAndUrl;
